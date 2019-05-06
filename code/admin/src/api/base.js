4c4bdc4c3df39a8e4749bfab54f49f08b8503e19
beijidehuo <beijidehuo1@gmail.com>
2019-05-06 23:25:14 +0800

    修改允许跨域和线上服务端ip地址

--- code/admin/src/api/base.js
+++ code/admin/src/api/base.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const isAppProd = process.env.app_config_env === 'prod'
 
 export default class Base {
-    static baseUrl = isProd ? 'http://dzblog.cn/api/admin' : '/admin_api'
+    static baseUrl = isProd ? 'http://45.32.34.153:3000/api/admin' : '/admin_api'
     static get = axios.get.bind(axios) // 获取
     static post = axios.post.bind(axios) // 添加
     static put = axios.put.bind(axios) // 更新
--- code/server/router/index.js
+++ code/server/router/index.js
@@ -4,6 +4,36 @@ import send from 'koa-send'
 import path from 'path'
 
 export default app => {
+  // 用户请求
+  router.all('/*', async (ctx, next) => {
+    // TODO::有的浏览器会是ctx.header
+    let { referer } = ctx.request.header;
+
+    if (referer) {
+      const parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+      const url = referer;
+      const urlResult = parse_url.exec(url);
+      const fields = ['url', 'scheme', 'slash', 'host', 'port', 'path', 'query', 'hash'];
+      const resField = [];
+      fields.forEach(function (field, i) {
+        resField[field] = urlResult[i];
+      });
+      let origin = resField.scheme + "://" + resField.host;
+      if (resField.port) {
+        origin += ":" + resField.port;
+      }
+
+      ctx.set("Access-Control-Allow-Origin", origin);
+      ctx.set("Access-Control-Allow-Credentials", "true");
+    } else {
+      ctx.set("Access-Control-Allow-Origin", "*");
+    }
+
+    ctx.set("Access-Control-Allow-Headers", 'Content-Type,Content-Length, Authorization,\'Origin\',Accept,X-Requested-With');
+    ctx.set("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    await next();
+  });
   // api
   let apiRouter = require('./routes/api').default
   router.use('/api', apiRouter.routes(), apiRouter.allowedMethods())
